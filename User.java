67648a5643d14441f09b5d69e157dc6a14d73231
NINJA-J <1605563934@qq.com>
2017-08-24 22:20:48 +0800

    Update User.java

--- User.java
+++ User.java
@@ -12,6 +12,45 @@ public class User {
 	int industryId;
 	int committeeId;
 	public User( String name, char gender, Date bDate, String address, String tel, User referrer, int iName, int cName ){
-		
+		this.name = name;
+		this.gender = gender;
+		this.birthDate = bDate;
+		this.address = address;
+		this.tel = tel;
+		this.referrer = referrer;
+		this.industryId = iName;
+		this.committeeId = cName;
+	}
+	
+	public String getName(){ return name; }
+	
+	public boolean isMale(){ return gender == 'M'; }
+	
+	public boolean isFemale(){ return gender == 'F'; }
+	
+	public Date getBirthDate(){ return birthDate; }
+	
+	public String getAddress(){ return address; }
+	
+	public String getTel(){ return tel; }
+	
+	public User getReferrer(){ return referrer; }
+	
+	public int getIndustry(){ return industryId; }
+	
+	public int getCommitteeId(){ return committeeId; }
+	
+	public String toString( String preBlock ){
+		return  preBlock + name + " : \n"+
+				preBlock + "    Gender    : " + gender + "\n" +
+				preBlock + "    BirthDate : " + birthDate + "\n" +
+				preBlock + "    Address   : " + address + "\n" +
+				preBlock + "    Tel       : " + tel + "\n" +
+				preBlock + "    Referrer  : " + referrer.getName() + "\n" +
+				preBlock + "    Industry  : " + industryId + "\n" +
+				preBlock + "    Committee : " + committeeId + "\n";
+	}
+	public String toString(){
+		return this.toString("");
 	}
 }
